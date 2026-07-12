314668f55eb7100b16294b77abe42edfe75e3209
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-12 22:38:56 +0900

    Update input.js

--- v1_1/js/input.js
+++ v1_1/js/input.js
@@ -1,1 +1,27 @@
-// input
+// ----------------
+// マウス操作
+// ----------------
+
+canvas.addEventListener("mousemove", function(event){
+
+    target.x = event.clientX;
+    target.y = event.clientY;
+
+});
+
+// ----------------
+// タッチ操作
+// ----------------
+
+canvas.addEventListener("touchmove", function(event){
+
+    event.preventDefault();
+
+    target.x = event.touches[0].clientX;
+    target.y = event.touches[0].clientY;
+
+},{
+
+    passive:false
+
+});
