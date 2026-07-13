2acaf2de8690a423bd2778b997eafa2447c1c50b
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-13 18:01:33 +0900

    Update renderer.js

--- v1_1/js/renderer.js
+++ v1_1/js/renderer.js
@@ -11,7 +11,64 @@ export class Renderer {
         const ctx = this.game.ctx;
         const canvas = this.game.canvas;
 
+        const maze = this.game.maze;
+        const eel = this.game.eel;
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // 壁
+        ctx.fillStyle = "#355";
+
+        for (const wall of maze.walls) {
+
+            ctx.fillRect(
+                wall.x,
+                wall.y,
+                maze.tileSize,
+                maze.tileSize
+            );
+
+        }
+
+        // ゴール
+        ctx.fillStyle = "gold";
+
+        ctx.beginPath();
+
+        ctx.arc(
+            maze.goal.x,
+            maze.goal.y,
+            maze.goal.radius,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+        // ウナギ
+        ctx.save();
+
+        ctx.translate(eel.x, eel.y);
+
+        ctx.rotate(eel.angle);
+
+        ctx.fillStyle = "#643";
+
+        ctx.beginPath();
+
+        ctx.ellipse(
+            0,
+            0,
+            maze.tileSize * 0.45,
+            maze.tileSize * 0.18,
+            0,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fill();
+
+        ctx.restore();
 
     }
 
