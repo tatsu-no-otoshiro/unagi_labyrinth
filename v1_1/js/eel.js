1aa9facc9a9dbeda07a94956ff543fd387a67958
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-13 17:55:05 +0900

    Update eel.js

--- v1_1/js/eel.js
+++ v1_1/js/eel.js
@@ -4,6 +4,26 @@ export class Eel {
 
         this.game = game;
 
+        this.x = 0;
+        this.y = 0;
+
+        this.angle = 0;
+
+        this.speed = 2.5;
+
+        this.radius = 8;
+
+    }
+
+    reset() {
+
+        const start = this.game.maze.start;
+
+        this.x = start.x;
+        this.y = start.y;
+
+        this.angle = 0;
+
     }
 
 }
