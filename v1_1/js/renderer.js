ee3391a9f29a12e66586266bba104b7e1289b185
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-19 02:02:09 +0900

    Update renderer.js

--- v1_1/js/renderer.js
+++ v1_1/js/renderer.js
@@ -224,10 +224,10 @@ export class Renderer {
 		const py = ux;
 
 		// 尾先の長さ
-		const tipLength = 20;
+		const tipLength = 16;
 
 		// 尾先の幅
-		const tipWidth = 3.5;
+		const tipWidth = 3;
 
 		ctx.beginPath();
 
